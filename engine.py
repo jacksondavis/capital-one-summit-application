54103c30e850c1aaa4c6fdaf04969f4ee04c4732
Jackson Davis <jaxtdavis@gmail.com>
2015-10-14 01:17:29 -0500

    Added aboutMe section, instagram functions, jQuery, and more

--- engine.py
+++ engine.py
@@ -1,7 +1,22 @@
 from instagram.client import InstagramAPI
+from secrets import INSTAGRAM_CODES
+import json
+import collections
 
-client_id = secrets.INSTAGRAM_CODES[0]
-client_secret = secrets.INSTAGRAM_CODES[1]
-access_token = secrets.INSTAGRAM_CODES[2]
-client_ip = secrets.INSTAGRAM_CODES[3]
-api = InstagramAPI(client_id=client_id, client_secret=client_secret,client_ips= client_ip,access_token= access_token)
+client_id = INSTAGRAM_CODES["CLIENT_ID"]
+client_secret = INSTAGRAM_CODES["CLIENT_SECRET"]
+access_token = INSTAGRAM_CODES["ACCESS_TOKEN"]
+client_ip = INSTAGRAM_CODES["CLIENT_IP"]
+api = InstagramAPI(client_id=client_id, client_secret=client_secret, client_ips= client_ip,access_token= access_token) 
+
+def get_insta_posts():
+	data = api.tag_recent_media(tag_name='CapitalOne')
+	return data[0]
+
+#Post = collections.namedtuple("Post", "likes", "caption")
+
+def get_media_info():
+	for post in get_insta_posts():
+		#api.media("Media")
+		print [post.like_count, post.caption]
+
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, flash, jsonify, render_template, redirect
 from flask_bootstrap import Bootstrap
+import engine
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route("/posts")
+def get_posts():
+    return engine.get_media_info()
+
+if __name__ == "__main__":
     app.run()
