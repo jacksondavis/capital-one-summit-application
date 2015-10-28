f933f7188ce6bf9e919b21a0554166a0ca1765ae
Jax Davis <jaxdavis@urhwirelessprvnat-172-18-73-142.near.illinois.edu>
2015-10-28 02:32:46 -0500

    Reindented html

--- engine.py
+++ engine.py
@@ -7,32 +7,40 @@ import json
 import random
 import decimal
 
+# Pulls keys from secrets.py and configures the InstagramAPI
 client_id = INSTAGRAM_CODES["CLIENT_ID"]
 client_secret = INSTAGRAM_CODES["CLIENT_SECRET"]
 access_token = INSTAGRAM_CODES["ACCESS_TOKEN"]
 client_ip = INSTAGRAM_CODES["CLIENT_IP"]
 api = InstagramAPI(client_id=client_id, client_secret=client_secret, client_ips= client_ip,access_token= access_token) 
+
+# Instantiates AlchemyAPI 
 alchemyapi = AlchemyAPI()
 
-#I debated using a database to hold the sentiments and data for each post,
-#However after running into issues with Heroku's PostgreSQL integration
-#And realizing the issues the actual database use would be light,
-#I opted to simply create a dictionary.
+# I debated using a database to hold the sentiments and data for each post,
+# However after running into issues with Heroku's PostgreSQL integration
+# And realizing the issues the actual database use would be light,
+# I opted to simply create a dictionary.
 post_data = {}
 
+# Method for randomly grabbing an AlchemyAPI key from secrets.py
 def get_random_alchemy_codes():
     alchemyCodes = ALCHEMY_CODES
     random.shuffle(alchemyCodes)
     return alchemyCodes
 
+# Grabs 20 recent-most Instagram posts tagged with CapitalOne
 def get_insta_posts():
 	data = api.tag_recent_media(tag_name='CapitalOne')
 	return data[0]
 
+# Gets Instagram info for user ID
 def get_user_info(id):
 	user = api.user(id)
 	return user
 
+# Tests a random AlchemyAPI key to see if it has not been overused,
+# Then runs the AlchemyAPI sentiment analysis on a post's caption
 def get_caption_sentiment(post):
 	response = ""
 	caption = post.caption
@@ -48,12 +56,14 @@ def get_caption_sentiment(post):
 				return response["docSentiment"]["type"]
 			elif response['status'] == 'ERROR':
 				print 'APIKey Used'
+				continue
 			else:
 				response = 'Error'
 				return response
 
 	return response
 
+# Creates a list of sentiment frequency information to be converted to JSON for d3.js 
 def get_sentiment_frequencies():
 	freqs = defaultdict(int)
 	for value in post_data.values():
